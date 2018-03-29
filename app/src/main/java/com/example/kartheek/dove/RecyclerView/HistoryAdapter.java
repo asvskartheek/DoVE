050ff17cb2fbb1667fd5064cfc0ace00abe6e57e
Sukruth Kartheek <sukruthkartheek@gmail.com>
2018-03-29 11:12:17 +0530

    Add Call button to Camera and History

--- app/src/main/java/com/example/kartheek/dove/RecyclerView/HistoryAdapter.java
+++ app/src/main/java/com/example/kartheek/dove/RecyclerView/HistoryAdapter.java
@@ -3,11 +3,13 @@ package com.example.kartheek.dove.RecyclerView;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kartheek.dove.R;
 import com.google.firebase.database.DataSnapshot;
@@ -56,11 +58,46 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryV
         }
 
         void bind(final int position){
-            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+            final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
             Intent intent = ((Activity) context).getIntent();
             final String type = intent.getStringExtra("type");
             final int number = intent.getIntExtra("number",0);
+            mPersonName.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            String user = dataSnapshot.child("Camera"+position).child("perName").getValue(String.class);
+                            assert user != null;
+
+
+                            int position_sudo = position+1;
+                            String child = type.concat(Integer.toString(number));
+                            String value = dataSnapshot.child(child).child("his"+position_sudo).getValue(String.class);
+                            System.out.println(value);
+                            assert value != null;
+                            if(!value.equals("u_1")&&!value.equals("u_10")){
+                                String phone_number = dataSnapshot.child("Users").child(user).child("phone").getValue(String.class);
+                                Uri uri = Uri.parse("tel:"+phone_number);
+                                Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+                                if (intent.resolveActivity(context.getPackageManager())!=null){
+                                    context.startActivity(intent);
+                                }
+                            }
+                            else {
+                                Toast.makeText(context,"This is a dummy user bruh!!",Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            });
             databaseReference.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
